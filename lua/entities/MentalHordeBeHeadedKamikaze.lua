f7f0ee747a17aff575fbd68cdd9ed1b4dd631872
KM_CM <send.nex@gmail.com>
2025-11-11 16:13:16 +0200

    Fixed the new Kamikaze hurt mechanism

--- lua/entities/MentalHordeBeHeadedKamikaze.lua
+++ lua/entities/MentalHordeBeHeadedKamikaze.lua
@@ -155,6 +155,7 @@ function ENT:Behaviour( MyTable )
 	if flLastHealth then
 		if flLastHealth > self:GetMaxHealth() * .5 && self:Health() <= self:GetMaxHealth() * .5 then
 			MyTable.AnimationSystemHalt( self, MyTable )
+			MyTable.flLastHealth = self:Health()
 			MyTable.PlaySequenceAndWait( self, self:LookupSequence( math.random( 1, 3 ) == 1 && "wound" || ( "wound" .. tostring( math.random( 2, 3 ) ) ) ), 1 )
 			return
 		end
